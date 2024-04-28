5959cd10c2c9e5ede24537a9147d584c56acd0d7
Lachlan Moreton <99296364@students.camre.ac.uk>
2024-04-28 19:53:35 +0100

    first build attempt

---
+++ main.asm
@@ -0,0 +1,192 @@
+;--------------------------------------------------------
+; File Created by SDCC : free open source ISO C Compiler 
+; Version 4.3.2 #14228 (MINGW64)
+;--------------------------------------------------------
+	.module main
+	.optsdcc -msm83
+	
+;--------------------------------------------------------
+; Public variables in this module
+;--------------------------------------------------------
+	.globl _main
+	.globl _getPlayerInput
+	.globl _performantDelay
+	.globl _set_sprite_data
+	.globl _set_bkg_tiles
+	.globl _set_bkg_data
+	.globl _wait_vbl_done
+	.globl _joypad
+	.globl _loopRunning
+	.globl _input
+	.globl _playerLocation
+;--------------------------------------------------------
+; special function registers
+;--------------------------------------------------------
+;--------------------------------------------------------
+; ram data
+;--------------------------------------------------------
+	.area _DATA
+_playerLocation::
+	.ds 2
+_input::
+	.ds 1
+;--------------------------------------------------------
+; ram data
+;--------------------------------------------------------
+	.area _INITIALIZED
+_loopRunning::
+	.ds 1
+;--------------------------------------------------------
+; absolute external ram data
+;--------------------------------------------------------
+	.area _DABS (ABS)
+;--------------------------------------------------------
+; global & static initialisations
+;--------------------------------------------------------
+	.area _HOME
+	.area _GSINIT
+	.area _GSFINAL
+	.area _GSINIT
+;--------------------------------------------------------
+; Home
+;--------------------------------------------------------
+	.area _HOME
+	.area _HOME
+;--------------------------------------------------------
+; code
+;--------------------------------------------------------
+	.area _CODE
+;main.c:13: void performantDelay(uint8_t delay){
+;	---------------------------------
+; Function performantDelay
+; ---------------------------------
+_performantDelay::
+	ld	c, a
+;main.c:15: for(i = 0; i < delay; i++){
+	ld	b, #0x00
+00103$:
+	ld	a, b
+	sub	a, c
+	ret	NC
+;main.c:16: wait_vbl_done();
+	call	_wait_vbl_done
+;main.c:15: for(i = 0; i < delay; i++){
+	inc	b
+;main.c:18: }
+	jr	00103$
+;main.c:20: void getPlayerInput(void){
+;	---------------------------------
+; Function getPlayerInput
+; ---------------------------------
+_getPlayerInput::
+;main.c:21: input = joypad();
+	call	_joypad
+	ld	hl, #_input
+;main.c:23: if(input & J_LEFT){
+	ld	(hl), a
+	bit	1, a
+	jr	Z, 00110$
+;main.c:24: playerLocation[0] -= 8;
+	ld	bc, #_playerLocation+0
+	ld	a, (bc)
+	add	a, #0xf8
+	ld	(bc), a
+	jr	00111$
+00110$:
+;main.c:26: else if(input & J_UP){
+	bit	2, a
+	jr	Z, 00107$
+;main.c:27: playerLocation[1] -= 8;
+	ld	bc, #_playerLocation+1
+	ld	a, (bc)
+	add	a, #0xf8
+	ld	(bc), a
+	jr	00111$
+00107$:
+;main.c:29: else if(input & J_RIGHT){
+	bit	0, a
+	jr	Z, 00104$
+;main.c:30: playerLocation[0] += 8;
+	ld	bc, #_playerLocation+0
+	ld	a, (bc)
+	add	a, #0x08
+	ld	(bc), a
+	jr	00111$
+00104$:
+;main.c:32: else if(input & J_DOWN){
+	bit	3, a
+	jr	Z, 00111$
+;main.c:33: playerLocation[1] += 8;
+	ld	bc, #_playerLocation+1
+	ld	a, (bc)
+	add	a, #0x08
+	ld	(bc), a
+00111$:
+;main.c:36: performantDelay(3);
+	ld	a, #0x03
+;main.c:37: }
+	jp	_performantDelay
+;main.c:39: void main(void){
+;	---------------------------------
+; Function main
+; ---------------------------------
+_main::
+;main.c:41: set_bkg_data(0, 16, BGSheet);
+	ld	de, #_BGSheet
+	push	de
+	ld	hl, #0x1000
+	push	hl
+	call	_set_bkg_data
+	add	sp, #4
+;main.c:42: set_bkg_tiles(0, 0, 20, 18, HubMap);
+	ld	de, #_HubMap
+	push	de
+	ld	hl, #0x1214
+	push	hl
+	xor	a, a
+	rrca
+	push	af
+	call	_set_bkg_tiles
+	add	sp, #6
+;main.c:44: set_sprite_data(0, 3, SpriteSheet);
+	ld	de, #_SpriteSheet
+	push	de
+	ld	hl, #0x300
+	push	hl
+	call	_set_sprite_data
+	add	sp, #4
+;c:\users\alive\desktop\gbdk\include\gb\gb.h:1804: shadow_OAM[nb].tile=tile;
+	ld	hl, #(_shadow_OAM + 2)
+	ld	(hl), #0x01
+;main.c:48: playerLocation[0] = 80;
+	ld	hl, #_playerLocation
+	ld	(hl), #0x50
+;main.c:49: playerLocation[1] = 80;
+	ld	hl, #(_playerLocation + 1)
+	ld	(hl), #0x50
+;main.c:54: SHOW_BKG;
+	ldh	a, (_LCDC_REG + 0)
+	or	a, #0x01
+	ldh	(_LCDC_REG + 0), a
+;main.c:55: SHOW_SPRITES;
+	ldh	a, (_LCDC_REG + 0)
+	or	a, #0x02
+	ldh	(_LCDC_REG + 0), a
+;main.c:56: DISPLAY_ON;
+	ldh	a, (_LCDC_REG + 0)
+	or	a, #0x80
+	ldh	(_LCDC_REG + 0), a
+;main.c:58: while(loopRunning){
+00101$:
+	ld	hl, #_loopRunning
+	bit	0, (hl)
+	ret	Z
+;main.c:59: getPlayerInput();
+	call	_getPlayerInput
+;main.c:61: }
+	jr	00101$
+	.area _CODE
+	.area _INITIALIZER
+__xinit__loopRunning:
+	.db #0x01	;  1
+	.area _CABS (ABS)
